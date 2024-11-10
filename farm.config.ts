fdcd0aa3634614a57758b7fbdbdb8de33a9d8b30
Dion Fananie <dionfananie@gmail.com>
2024-11-10 18:26:22 +0700

    feat(init): udpate config

--- farm.config.ts
+++ farm.config.ts
@@ -2,18 +2,18 @@ import { defineConfig } from "@farmfe/core";
 import { middleware } from "./src/server/middleware";
 import farmPluginPostcss from "@farmfe/js-plugin-postcss";
 export default defineConfig({
-  compilation: {
-    input: {
-      index_client: "./index.html",
-    },
-    output: {
-      path: "./build",
-    },
-  },
-  server: {
-    hmr: true,
-    cors: true,
-    middlewares: middleware,
-  },
+  // compilation: {
+  //   input: {
+  //     index_client: "./index.html",
+  //   },
+  //   output: {
+  //     path: "./build",
+  //   },
+  // },
+  // server: {
+  //   hmr: true,
+  //   cors: true,
+  //   middlewares: middleware,
+  // },
   plugins: ["@farmfe/plugin-react", "@farmfe/plugin-sass", farmPluginPostcss()],
 });
